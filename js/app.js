4a99ab3457097a6679f40518e8b52f9070d8e710
Ivan Irmiev <irmiev@gmail.com>
2026-02-07 18:51:36 +0000

    Added a title to the trap.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
 const QUERY_PARAM = "payload";
-const HEADER_ID = "clue-header";
+const TRAP_HEADER = "trap-header";
+const TRAP_TITLE = "trap-title";
 const VIDEO_ID = "video";
 
 window.onload = function () {
     let payload = new URL(window.location).searchParams.get(QUERY_PARAM);
-    let header = document.getElementById(HEADER_ID);
+    let header = document.getElementById(TRAP_HEADER);
+    let title = document.getElementById(TRAP_TITLE);
     let frame = document.getElementById(VIDEO_ID);
 
-    if(payload == null){
+    if (payload == null) {
         header.innerText = "What are you looking at?";
         return 0;
     }
-    
+
     let data = JSON.parse(this.atob(payload));
 
     let youtubeLink = data.link;
     let iframe = `<iframe 
-    width="${window.innerWidth}" height="${window.innerWidth/2}" 
+    width="${window.innerWidth}" height="${window.innerWidth / 2}" 
     src="${youtubeLink}?autoplay=1&mute=1"
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
@@ -24,5 +26,6 @@ window.onload = function () {
     </iframe>`;
 
     header.innerText = `You fell into trap #${data.trapId}`;
+    title.innerText = `"${data.trapTitle}"`;
     frame.innerHTML = iframe;
 }
